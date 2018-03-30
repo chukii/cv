8d44783995b550a6150364b110c8af2263bb8978
chukii <mrlq@protonmail.com>
2018-03-30 18:26:44 +0200

    .

--- main.py
+++ main.py
@@ -7,15 +7,19 @@ frame = tk.Frame(root)
 
 frame.pack()
 
-def show():
+def show_WIT():
 	T=Text(root)
 	T.pack()
 	T.insert(tk.END,"This is my CV app")
-	root.mainloop()
+	#root.mainloop()
 
-
-btn_about_me=tk.Button(frame,text='About Me',fg='blue')
-btn_what_is_this=tk.Button(frame,text='What is this',fg='green',command=show)
+def show_AM():
+	T=Text(root)
+	T.pack()
+	T.insert(tk.END,"I am Miguel Ramos, a Systems Engineer")
+	#root.mainloop()
+btn_about_me=tk.Button(frame,text='About Me',fg='blue',command = show_AM)
+btn_what_is_this=tk.Button(frame,text='What is this',fg='green',command=show_WIT)
 btn_about_me.pack(side=tk.LEFT)
 btn_what_is_this.pack(side=tk.LEFT)
 root.mainloop()
